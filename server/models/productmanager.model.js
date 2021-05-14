41d9e23f1ae939452bd25c578ce350f312f8639a
Chris Galera <galera.chris@yahoo.com>
2021-05-13 22:01:07 -0500

    Passed test in Postman for creating product manager

--- server/models/productmanager.model.js
+++ server/models/productmanager.model.js
@@ -3,18 +3,18 @@ const mongoose = require('mongoose');
 const ProductmanagerSchema = new mongoose.Schema({
     title: {
         type: String,
-        required: [true, "This is a required field."],
+        // required: [true, "This is a required field."],
         // minlength: [3, ""]
     }, 
     //later on, we should make this an array of strings?
     price: {
         type: Number,
-        required: [true, "This is a required field."],
+        // required: [true, "This is a required field."],
         // required: [true, ""]
     },
     description: {
         type: String, 
-        required: [true, "This is a required field."],
+        // required: [true, "This is a required field."],
         // minlength: [5, ""]
     }
 }, 
